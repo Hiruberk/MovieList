82066b9237a51e1a9026f08ca93947b8bf00b2f8
Nate <nsdavis@mtu.edu>
2021-01-28 16:06:42 -0500

    Needs to have a Sorted List

--- MovieListLab10/Movie.cs
+++ MovieListLab10/Movie.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace MovieListLab10
 {
-    class Class1
+    class Movie
     {
+        public string Title { get; set; }
+        public string Category { get; set; }
+
+
+
+        public Movie(string Title, string Category)
+        {
+            this.Title = Title;
+            this.Category = Category;
+
+        }
+
+
+
+        public void PrintMovie()
+        {
+            Console.WriteLine("----------------Movie List-------------");
+            Console.WriteLine($"Title: {Title}    Genre: {Category}");
+            Console.WriteLine("---------------------------------------");
+        }
+
+       //public List<movies> FindCategory(string Category)
+       // {
+
+       // } 
+
     }
 }
--- MovieListLab10/Program.cs
+++ MovieListLab10/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
 
 namespace MovieListLab10
 {
@@ -6,7 +9,58 @@ namespace MovieListLab10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Movie List Application!\n");
+            Console.WriteLine("There are 20 movies in this list.");
+
+            List<Movie> movies = new List<Movie>();
+
+            //Animated Movies
+            movies.Add(new Movie("Toy Story 4", "Animated"));
+            movies.Add(new Movie("Inside Out", "Animated"));
+            movies.Add(new Movie("Coco", "Animated"));
+            movies.Add(new Movie("Zootopia", "Animated"));
+            movies.Add(new Movie("Incredibles 2", "Animated"));
+
+            //Drama Movies
+            movies.Add(new Movie("Black Panther", "Drama"));
+            movies.Add(new Movie("Parasite", "Drama"));
+            movies.Add(new Movie("Knives Out", "Drama"));
+            movies.Add(new Movie("Lady Bird", "Drama"));
+            movies.Add(new Movie("Citizen Kane", "Drama"));
+
+            //Horror Movies
+            movies.Add(new Movie("The Exorcist", "Horror"));
+            movies.Add(new Movie("Hereditary", "Horror"));
+            movies.Add(new Movie("The Conjuring", "Horror"));
+            movies.Add(new Movie("The Shining", "Horror"));
+            movies.Add(new Movie("The Ring", "Horror"));
+
+            //Sci-Fi Movies
+            movies.Add(new Movie("2001: A Space Odyssey", "Sci-Fi"));
+            movies.Add(new Movie("Aliens", "Sci-Fi"));
+            movies.Add(new Movie("Blade Runner", "Sci-Fi"));
+            movies.Add(new Movie("Interstellar", "Sci-Fi"));
+            movies.Add(new Movie("Back to the Future", "Sci-Fi"));
+
+            
+            while (true)
+            {
+                Console.WriteLine("What category are your interested in? (Animated, Drama, Horror, Sci-Fi");
+
+
+
+                //for (int i = 0; i < movies.Count; i++)
+                //{
+                //    Movie cinema = movies[i];
+                //    Console.WriteLine($"Index: {i} | Title: {cinema.Title} | Genre: {cinema.Category}");
+                //}
+
+                Console.WriteLine("\nWould you like to look at another genre? (y/n)");
+                if(Console.ReadLine().ToLower() == "n")
+                {
+                    break;
+                }
+            }
         }
     }
 }
